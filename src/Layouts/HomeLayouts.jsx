4f094862254b12eeb5d9d14061a18734c329076a
ASHIK-KHAN-ATUL <ashikkhan693693@gmail.com>
2025-05-09 20:52:20 +0600

    1 new commit

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -3,7 +3,11 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 
+
 const HomeLayouts = () => {
+
+    // Hi i am ashik
+
     return (
         <div>
             <Navbar></Navbar>
